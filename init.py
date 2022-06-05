56df092257f89478a80068adf19c912108de4228
Huisu Lim <4922865@gmail.com>
2022-06-05 16:10:21 +0900

    add datas that I have to import

--- init.py
+++ init.py
@@ -1,8 +1,12 @@
 #for initalize service
+#connect to excle
+  #load adjection
+from openpyxl import load_workbook
+EXCLEconnect=load_workbook("")
 #connect to db
 import cx_Oracle
 ID="user"
 PASSWORD="1234"
 LOCALHOST="localhost/orcl"
-connect=cx_Oracle.conenct(ID,PASSWORD,LOCALHOST)
-cursur=connect.cursor()
+DBconnect=cx_Oracle.conenct(ID,PASSWORD,LOCALHOST)
+cursur=DBconnect.cursor()
