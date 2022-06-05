726d61fb863713e9cc3b0786acb068932cad9c65
Huisu Lim <4922865@gmail.com>
2022-06-05 15:57:54 +0900

    connect db and make pip list

--- init.py
+++ init.py
@@ -1,1 +1,8 @@
 #for initalize service
+#connect to db
+import cx_Oracle
+ID="user"
+PASSWORD="1234"
+LOCALHOST="localhost/orcl"
+connect=cx_Oracle.conenct(ID,PASSWORD,LOCALHOST)
+cursur=connect.cursor()
